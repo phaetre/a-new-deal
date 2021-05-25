02a6f3455dd03e942c3a326795d21bcd56965c79
phaetre <84492954+phaetre@users.noreply.github.com>
2021-05-25 09:53:03 -0600

    Update main2.js

--- main2.js
+++ main2.js
@@ -9,7 +9,6 @@ function button(innerHTML, title, onClick = () => {}, classes = []) {
 	}
 	return j;
 }
-document.title = 'A New Deal';
 function moneyFormat(money) {
 	let r = money % 1;
 	r || (r += '00');
@@ -19,10 +18,31 @@ function moneyFormat(money) {
 }
 function moneyUpdate() {
 	m.innerHTML = 'Money: $' + moneyFormat(money);
+	localStorage.setItem('money', money);
 }
 function stockUpdate() {
-	s.innerHTML = `Stocks: ${stock}`;
+	s.innerHTML = `Stocks owned: ${stock}`;
 	t.innerHTML = `Price per Stock: $${moneyFormat(ticker.stocks.main)}`;
+	d.innerHTML = `${date[1]}/${date[2]}/${date[0]}`;
+	localStorage.setItem('stock', stock);
+	localStorage.setItem('prices', ticker.focusStock);
+	localStorage.setItem('day', day);
+	localStorage.setItem('date', date);
+}
+function nextDay(date) {
+	let m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+		d = [...date],
+		ly = (d[0] % 4 ? 0 : (d[0] % 100 ? 1 : (d[0] % 400 ? 1 : 0)));
+	d[2]++;
+	if (d[2] > m[d[1] - 1] + (ly && d[1] == 2)) {
+		d[1]++;
+		d[2] = 1;
+		if (d[1] > 12) {
+			d[0]++;
+			d[1] = 1;
+		}
+	}
+	return d;
 }
 var datum = fetch('data2.txt').then(i=>i.json()),
 	ticker = new Ticker({'main': 41.22}, 100, 60, 'main', window.innerWidth/2.01, datum),
@@ -34,15 +54,22 @@ var datum = fetch('data2.txt').then(i=>i.json()),
 	m = document.createElement('p'),
 	s = document.createElement('p'),
 	t = document.createElement('p'),
+	d = document.createElement('p'),
 	c = document.createElement('div'),
 	c2 = document.createElement('div'),
 	c3 = document.createElement('div'),
 	c4 = document.createElement('div'),
 	c5 = document.createElement('div'),
 	c6 = document.createElement('div'),
-	data = fetch('data.txt').then(i=>i.json()).then(q=>(q.map(i=>{let j = button(i[0], i[1]); c4.appendChild(j); return j}))),
+	c7 = document.createElement('div'),
+	data = fetch('data.txt').then(i=>i.json()).then(q=>(q.map(i=>{
+		let j = button(i[0] + '\nCost (in-game): $' + i[7], i[1], () => {if (money > i[7]) {ticker.val *= (1 + q[2] / 100) || 1; ticker.add += q[3] || 0; j.classList.add('used'); log.innerHTML += `<br>${i[0]}:<br>${i[1]}`}}, ['locked']); 
+		c4.appendChild(j); 
+		return [j, [q[4], q[5], q[6]]]}))),
 	num = 1,
 	stock = 0,
+	day = 0,
+	date = [1932, 7, 8],
 	buy = button('BUY', 
 				 'Buys the currently selected quantity of stock if you can afford it.', 
 				 () => {let m = (ticker.stocks.main * 100 | 0) / 100, 
@@ -92,7 +119,7 @@ var datum = fetch('data2.txt').then(i=>i.json()),
 	pressure = 0,
 	cT = undefined,
 	tab = 'Upgrades',
-	tabs = (['Upgrades', 'Log', 'Key Concept']).map(i=>button(
+	tabs = (['Upgrades', 'Log', 'Key Concept', 'Help']).map(i=>button(
 		i, 
 		`Set buy/sell quantity to ${i}.`, 
 		()=>{tab = i; 
@@ -105,10 +132,14 @@ var datum = fetch('data2.txt').then(i=>i.json()),
 			 cT = tabs[j]; 
 			 cT.style.background = '#111'; 
 			 [...views[i].children].forEach(q => q.hidden = false)})),
-	views = {'Upgrades': c4, 'Log': c5, 'Key Concept': c6},
+	views = {'Upgrades': c4, 'Log': c5, 'Key Concept': c6, 'Help': c7},
 	log = document.createElement('textarea'),
-	key = document.createElement('p');
-key.innerHTML = 'This project relates entirely to Key Concept 7.1, section III, which states:<br>"During the 1930s, policymakers responded to the mass unemployment and social upheavals of the Great Depression by transforming the U.S. into a limited welfare state, redefining the goals and ideas of modern American liberalism."<br>'
+	key = document.createElement('p'),
+	help = document.createElement('p'),
+	dividend = 0.3;
+key.innerHTML = 'This project relates entirely to Key Concept 7.1, section III, which states:<br>"During the 1930s, policymakers responded to the mass unemployment and social upheavals of the Great Depression by transforming the U.S. into a limited welfare state, redefining the goals and ideas of modern American liberalism."<br>This project presents 9 different New Deal acts / programs, with brief descriptions of each of them. For more information about New Deal programs, go to <a href="https://livingnewdeal.org/">this site</a>, where most of this information was sourced from.<br><br>In terms of historical accuracy, the initial stock ticker data (when the game starts) is accurate (and sourced from <a href="https://www.macrotrends.net/2484/dow-jones-crash-1929-bear-market">here</a>), while all of the stock data after that is randomly generated, and an inaccurate simulator of actual stocks. The upgrades are actual historical policies that happened, and the dates are accurate, however unless it says a specific effect in the description for an upgrade, all of the effects are made up.';
+help.innerHTML = 'Buy and sell stocks to make money, then use that to buy upgrades. You will also passively make money for every stock you have. Hover over each upgrade to learn more about that New Deal policy!';
+log.innerHTML = 'When you purchase or unlock upgrades, their descriptions will be added here.';
 points[0].style.background = '#111';
 tabs[0].style.background = '#111';
 nF = points[0];
@@ -118,12 +149,14 @@ rightDiv.classList.add('page');
 c.classList.add('c');
 c2.classList.add('c');
 c3.classList.add('c');
+document.title = 'A New Deal';
 //leftDiv.style.width = rightDiv.style.width = '45%';
 //m.innerHTML = '$'+money;
 moneyUpdate();
 stockUpdate();
 mainDiv.appendChild(leftDiv);
 mainDiv.appendChild(rightDiv);
+leftDiv.appendChild(d);
 leftDiv.appendChild(ticker.canvas);
 leftDiv.appendChild(m);
 leftDiv.appendChild(s);
@@ -138,21 +171,24 @@ for (let i of tabs) {
 }
 log.hidden = true;
 key.hidden = 1;
+help.hidden = 1;
 key.classList.add('blah');
 log.disabled = 'disabled';
 c5.appendChild(log);
 c6.appendChild(key);
+c7.appendChild(help);
 leftDiv.appendChild(c);
 leftDiv.appendChild(c2);
 rightDiv.appendChild(c3);
 rightDiv.appendChild(c4);
 rightDiv.appendChild(c5);
 rightDiv.appendChild(c6);
+rightDiv.appendChild(c7);
 document.body.appendChild(mainDiv);
 function tick(l) {
-	let a = ticker.tick(frames, pressure);
+	let a = ticker.tick(frames, pressure, stock);
 	ticker.render(a);
-	a && (stockUpdate(), (pressure *= 0.7));
+	a && ((money += Math.max(dividend * stock * ((ticker.focusStock[ticker.focusStock.length - 1] || 0) - (ticker.focusStock[ticker.focusStock.length - 2] || 0)), 0)), stockUpdate(), moneyUpdate(), (pressure *= 0.7), day++, (date = nextDay(date)), (()=>{for (let i in data){let j = i[1];if (j[0] == date[0] && j[1] == date[1] && j[2] == date[2]) {i[1].classList.remove('locked')}}})(), console.log(date));
 	frames++;
 	requestAnimationFrame(tick);
 }
